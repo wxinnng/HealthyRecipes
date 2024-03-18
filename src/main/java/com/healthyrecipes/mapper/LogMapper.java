0ca4afe42161b88b2f716caf66ddb8c3ad988986
WangXing <13493036+WX_Space1@user.noreply.gitee.com>
2024-03-18 10:57:52 +0000

    update src/main/java/com/healthyrecipes/mapper/LogMapper.java.
    Signed-off-by: WangXing <13493036+WX_Space1@user.noreply.gitee.com>

--- src/main/java/com/healthyrecipes/mapper/LogMapper.java
+++ src/main/java/com/healthyrecipes/mapper/LogMapper.java
@@ -1,13 +1,30 @@
 package com.healthyrecipes.mapper;
 
+import com.healthyrecipes.pojo.entity.LogContent;
+import com.healthyrecipes.pojo.entity.Topic;
+import com.healthyrecipes.pojo.query.LogQuery;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 /**
  * @Author:86198
  * @DATE:2024/3/17 20:54
- * @DESCRIPTION:  Log Mapper
- * @VERSION:1.0
+@@ -10,4 +15,17 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface LogMapper {
+    /**
+     * 分页查询log列表
+     * @param logQuery 查询条件
+     * @return java.util.List<com.healthyrecipes.pojo.entity.LogContent>
+     */
+    List<LogContent> queryLogByPage(LogQuery logQuery);
+
+    /**
+     * 获取Topic
+     * @param topic 查询条件
+     * @return java.util.List<com.healthyrecipes.pojo.entity.topic>
+     */
+    List<Topic> getTopicList(Topic topic);
 }
