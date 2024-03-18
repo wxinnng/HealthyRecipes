1d8108fe10451a0e3e6948fc17bee8c2b0683f22
WangXing <13493036+WX_Space1@user.noreply.gitee.com>
2024-03-18 10:51:48 +0000

    update src/main/java/com/healthyrecipes/service/LogService.java.
    Signed-off-by: WangXing <13493036+WX_Space1@user.noreply.gitee.com>

--- src/main/java/com/healthyrecipes/service/LogService.java
+++ src/main/java/com/healthyrecipes/service/LogService.java
@@ -1,6 +1,11 @@
 package com.healthyrecipes.service;
 
+import com.healthyrecipes.pojo.entity.LogContent;
+import com.healthyrecipes.pojo.entity.Topic;
+import com.healthyrecipes.pojo.query.LogQuery;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * @Author:86198
